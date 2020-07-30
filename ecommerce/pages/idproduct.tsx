38c15bc4fe29b9683ef41b9db5bc7d721f432e05
jowpurcinelli <jaopedropurcinelli@gmail.com>
2020-07-30 17:53:37 -0300

    interactingwithAPI

--- ecommerce/pages/idproduct.tsx
+++ ecommerce/pages/idproduct.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import Stripe from 'stripe';
-import { getStaticPaths, GetStaticPaths } from 'next';
+import { GetStaticPaths } from 'next';
 
+import stripeConfig from '../config/stripe';
 //this will call the stripe api and return it's paths
 // you can find the api details  in the config folder
-const  getStaticPats: GetStaticPaths = async ( ) => {
-    const stripe = new Stripe( )
+
+
+
+const Product : React.FC = ( ) => {
+    return <h1>Products</h1>
+}
+
+
+
+//need to be exported so the server can recognize it, I think    ':)
+export const  getStaticPaths: GetStaticPaths = async ( ) => {
+    const stripe = new Stripe( stripeConfig.secretKey, {
+        apiVersion: '2020-03-02',
+    });
     
+    const products = await stripe.products.list( );
     
+    console.log(products.data);
+
+
     return {
         paths: [ ], //array type
         fallback: false,
-    }
-}
+    };
+};
+
+
+
+export default Product;
--- ecommerce/pages/product.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Product: React.FC = ( ) => {
-    return <h1>Products</h1>
-};
-
-export default Product;
